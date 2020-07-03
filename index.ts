beff7d122c9d7ed6cd8a41a55b7280ca2f3aaf7f
Mohan Rathod <mohan.rathod@robosoftin.com>
2020-07-03 18:04:31 +0530

    Fixed for the exception show on next line of console

--- index.ts
+++ index.ts
@@ -2,10 +2,14 @@ import './style.css';
 
 import { fromEvent, of, merge, empty, concat, defer } from 'rxjs';
 import { delay, map, mergeMap, tap, debounceTime, distinctUntilChanged, mapTo, filter, share, switchAll, switchMap } from 'rxjs/operators';
+import { ifElse } from 'ramda';
 import { format } from 'date-fns';
 
 // track in progress saves
 let savesInProgress = 0;
+
+let historyCursor = 0;
+let historyData = [];
 
 // references
 const input = document.getElementById('note-input');
@@ -18,15 +22,22 @@ const keyup$ = fromEvent(input, 'keyup');
 // @ts-ignore
 const addText = (elId, text) => {
     console.log('value : ' + text);
-    let textInput = text.split(".");
+    let textInput = text.split(" ");
     console.log("textInput: " + textInput[0]);
     if(textInput[0] === "console" || textInput[0] === "function"){
         console.log("INside eval");
-        // @ts-ignore
-        document.getElementById(elId).value += eval(text)+ "\n";
+        try {
+          setTimeout(function() {
+            // @ts-ignore
+            document.getElementById(elId).value += eval("("+text+")")+ "\n";
+          }, 1000);
+        } catch (e) {
+          alert( "won't work" );
+        }
+        
     }else {
         console.log("INside findResult");
-        let finalVal = findResult(text);
+        let finalVal = findResult(elId, text);
         // @ts-ignore
         document.getElementById(elId).value += finalVal + "\n";
     }
@@ -34,20 +45,34 @@ const addText = (elId, text) => {
 }
 
 // @ts-ignore
-const findResult = (value) => {
+const findResult = (elId, value) => {
   var valueToEval = value;
   console.log("valueToEval : " + valueToEval);
-  let result = eval(valueToEval);
-  console.log("resultEval : " + result);
-  return result;
+  let result = "";
+  try {
+    if(valueToEval instanceof Function){
+      var fn = "fn = " + "'"+valueToEval+"'";
+      result = eval(fn);
+    }else{
+      result = eval(valueToEval);
+    }
+    
+    // result =  'use strict;return (' + valueToEval + ')';
+    console.log("resultEval : " + result);
+    return result;
+  } catch (e) {
+    // @ts-ignore
+    document.getElementById(elId).value += e + "\n";
+  }
+  
 }
 
 // fake save request
 // @ts-ignore
 const saveChanges = value => {
     console.log("value : " + value);
-  addText('note-input', value);
-  return of(value).pipe(delay(1500))
+    addText('note-input', value);
+    return of(value).pipe(delay(1500))
 };
 
 // @ts-ignore
@@ -60,21 +85,62 @@ const getContinents = keys => [
   'array'
 ].filter(e => e.indexOf(keys.toLowerCase()) > -1);
 
+const enterEntered = () => {}
+
 // @ts-ignore
 const fakeContinentsRequest = keys => of(getContinents(keys))
   .pipe(
     tap(_ => console.log(`API CALL at ${new Date()}`))
   );
 
+// @ts-ignore
+const checkKeyPressed = (keyCode) => {
+  console.log("Inside checkKeyPressed");
+  if(keyCode == 13){ // enter
+    console.log("enter");
+    return;
+  }else if(keyCode == 13 && keyCode == 16){ // enter and shift
+    console.log("enter and shift");
+  }else if(keyCode == 38) { // UP arrow
+    console.log("UP arrow");
+    if (historyCursor < 0) {
+      historyCursor = 0;
+      return;
+    }
+    historyCursor++;
+    // @ts-ignore
+    let value = history[historyCursor];
+    // @ts-ignore
+    document.getElementById(elId).value += value + "\n";
+    return;
+  }else if(keyCode == 40) { // DOWN arrow
+    console.log("DOWN arrow");
+    if (historyCursor == historyCursor) {
+      historyCursor = historyCursor;
+      return;
+    }
+    historyCursor--;
+    // @ts-ignore
+    let value = history[historyCursor];
+    // @ts-ignore
+    document.getElementById(elId).value += value + "\n";
+    return;
+  }else {
+    console.log("else...");
+  }
+}
+
 /**
  * Trigger a save when the user stops typing for 200ms
  * After new data has been successfully saved, so a saved
  * and last updated indicator.
  */
-const inputToSave$ = keyup$
-  .pipe(
-    
+const inputToSave$ = keyup$.pipe(
     //debounceTime(200),
+    // @ts-ignore
+    // mergeMap((e: KeyboardEvent) => checkKeyPressed(e.keyCode)),
+    // @ts-ignore
+    // checkKeyPressed((e: KeyboardEvent) => e.keyCode),
     filter((e: KeyboardEvent) => e.keyCode === 13),
     // @ts-ignore
     map(e => e.target.value),
@@ -85,6 +151,7 @@ const inputToSave$ = keyup$
   );
 
 const savesInProgress$ = inputToSave$.pipe(
+
   mapTo(of('Saving')),
   tap(_ => savesInProgress++)
 );
@@ -104,6 +171,7 @@ const savesCompleted$ = inputToSave$.pipe(
 );
 
 merge(
+  // checkKeyPressed$,
   savesInProgress$,
   savesCompleted$
 ).pipe(
